036fbeaa3e0c388e9dece3045b3b0ab37e4bc4ad
Danilo <daniloverhaert@gmail.com>
2018-05-01 13:32:32 -0700

    Updated documentation

--- kernel/src/hil/analog_comparator.rs
+++ kernel/src/hil/analog_comparator.rs
@@ -12,22 +12,26 @@ pub trait AnalogComparator {
     /// that when the AC is always on the power dissipation will be higher.
     fn initialize_acifc(&self) -> ReturnCode;
 
-    /// Do a comparison of two inputs. Output will be 1 when one is higher than
-    /// the other, and 0 otherwise. Specifically, the output is 1 when Vinp>Vinn
-    /// (Vin positive > Vin negative), and 0 if Vinp < Vinn.
+    /// Do a comparison of two inputs, depending on the AC chosen. Output will
+    /// be True (1) when one is higher than the other, and False (0) otherwise.
+    /// Specifically, the output is True when Vp>Vn (Vin positive > Vin 
+    /// negative), and False if Vp < Vn.
     fn comparison(&self, usize) -> bool;
 
-    /// Do a comparison of three input voltages. Two ACs, ACx and ACx+1 are
-    /// grouped for this comparison. The sources of the negative input of ACx
+    /// Do a comparison of three input voltages. Two analog comparators, ACx and ACx+1 are
+    /// grouped for this comparison depending on the window chosen . They each have a positive and negative input: we
+    /// define these respectively as (Vp_x and Vn_x) for ACx and (Vp_x+1 and Vn_x+1) for ACx+1. 
+    /// The sources of the negative input of ACx
     /// (Vn_x) and the positive input of ACx+1 (Vp_x+1) must be connected
-    /// together externally. These form the common voltage Vcommon. The other
-    /// two remaining sources, being the positive input of ACx (Vp_x) and
-    /// negative input of ACx+1 (Vn_x+1) then define an upper and a lower bound
-    /// of a window. The result then depends on Vcommon lying inside of outside
-    /// of this window. When the voltage of Vcommon lies inside the window
-    /// defined by the positive input of ACx and the negative input of ACx+1,
-    /// the output will be 1; it will be 0 if it is outside of the window.
-    /// Specifically, the output will be 1 when Vn_x+1 < Vcommon < Vp_x, and 0
+    /// together externally as a prerequisite to use the windowed mode. These
+    /// then together form the common voltage Vcommon. 
+    /// The way the windowed mode works is then as follows. The two remaining sources, being the positive input of ACx (Vp_x) and
+    /// negative input of ACx+1 (Vn_x+1) define an upper and a lower bound
+    /// of a window. The result of the comparison then depends on Vcommon lying inside of outside
+    /// of this window. 
+    /// When the value of Vcommon lies inside this window,
+    /// the output will be True (1); it will be False (0) if it lies outside of the window.
+    /// Specifically, the output will be True when Vn_x+1 < Vcommon < Vp_x, and False
     /// if Vcommon < Vn_x+1 or Vcommon > Vp_x.
     fn window_comparison(&self, usize) -> bool;
 
